80e84e25f38c97c4ae1f3dfe803304e18754a787
Raymond Chong <mathsgod@yahoo.com>
2023-08-17 15:39:53 +0800

    🔧 修正(_List.php)：新增disabled方法以設置disabled屬性 🔧 修正(_List.php)：新增dynamic方法以設置dynamic屬性

--- src/Inputs/_List.php
+++ src/Inputs/_List.php
@@ -5,4 +5,22 @@ namespace FormKit\Inputs;
 
 class _List extends \FormKit\FormKitInputs
 {
+
+    public function disabled(bool $disabled = true)
+    {
+        if ($disabled) {
+            $this->setAttribute("disabled", "");
+        } else {
+            $this->removeAttribute("disabled");
+        }
+    }
+
+    public function dynamic(bool $dynamic = true)
+    {
+        if ($dynamic) {
+            $this->setAttribute("dynamic", "");
+        } else {
+            $this->removeAttribute("dynamic");
+        }
+    }
 }
